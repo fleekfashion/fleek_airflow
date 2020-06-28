366f8a6ad49eff9dd68a59d09d72d93471d36b94
ghodouss <ghodouss@usc.edu>
2020-06-28 18:21:15 -0500

    Changed default n_product_stats to 0 from 1

--- src/template/upload_new_active_products.sql
+++ src/template/upload_new_active_products.sql
@@ -11,10 +11,10 @@ WITH emb_data AS (
 SELECT 
   i.*,
   e.product_embedding,
-  1 as n_likes,
-  1 as n_views,
-  1 as n_add_to_cart,
-  1 as n_conversions
+  0 as n_likes,
+  0 as n_views,
+  0 as n_add_to_cart,
+  0 as n_conversions
 FROM emb_data e
 INNER JOIN `{{params.new_info_table}}` i
 ON e.product_id = i.product_id
