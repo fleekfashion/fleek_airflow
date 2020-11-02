6c52c797bd57077583561a5770e507039b6cf17c
ghodouss <ghodouss@usc.edu>
2020-11-01 21:05:13 -0600

    massive deletion

--- src/defs/bq/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import personalization
-from . import gcs_exports
-from . import gcs_imports
-from . import user_data
--- src/subdags/table_setup/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import personalization
-from . import gcs_exports
-from . import gcs_imports
-from . import postgre_personalization
-from . import user_data
