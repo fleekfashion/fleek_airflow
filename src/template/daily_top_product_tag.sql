033bccf0d0ff28cf8ac90e2fd1806e64855015af
Kian Ghodoussi <ghodouss@usc.edu>
2020-11-20 17:19:38 -0600

    Only write active products to top products and require at least 1 like/add to cart (#34)

--- src/template/daily_top_product_tag.sql
+++ src/template/daily_top_product_tag.sql
@@ -2,6 +2,7 @@ WITH top_p AS (
   SELECT *
   FROM {{ params.active_table }} 
   WHERE n_views > {{ params.min_views }} 
+    AND n_likes + n_add_to_cart > 1
   ORDER BY (n_likes + n_add_to_cart) / n_views DESC
   LIMIT {{ params.limit }} 
 )
