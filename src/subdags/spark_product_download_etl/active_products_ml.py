46cde9c7a3aa00daa97f120c6ad247b996e5fc3e
ghodouss <ghodouss@usc.edu>
2020-11-20 11:56:24 -0600

    added top product tag sql

--- src/subdags/spark_product_download_etl/active_products_ml.py
+++ src/subdags/spark_product_download_etl/active_products_ml.py
@@ -71,8 +71,21 @@ def get_operators(dag: DAG_TYPE) -> dict:
         num_workers=4,
     )
 
+    top_product_tag = spark_sql_operator(
+        task_id="daily_top_product_tag",
+        dag=dag,
+        sql="template/daily_top_product_tag.sql",
+        params={
+            "active_table": pcdefs.get_full_name(pcdefs.ACTIVE_PRODUCTS_TABLE),
+            "min_views": 2,
+            "limit": 1000,
+            "tag": "top_product"
+        },
+        local=True
+    )
     
     head >> compute_product_similarity >> process_similar_products >> tail
     head >> product_recs >> tail
+    head >> top_product_tag >> tail
 
     return {"head": head, "tail": tail}
