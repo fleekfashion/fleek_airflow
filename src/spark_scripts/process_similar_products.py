b861f73d5e15f99bc0822c27b5ce6d959d896065
ghodouss <ghodouss@usc.edu>
2020-12-27 14:32:30 -0600

    modified process similar items as well

--- src/spark_scripts/process_similar_products.py
+++ src/spark_scripts/process_similar_products.py
@@ -6,7 +6,6 @@ from pyspark import SparkContext
 
 # Databricks notebook source
 from pyspark.sql import functions as F
-from pyspark.sql.types import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--json", type=str, required=True)
@@ -23,24 +22,39 @@ try:
     sqlContext = SQLContext(1)
     sc = SparkContext()
     spark = SparkSession(sc)
-except:
+except Exception:
     pass
+
+## Run SQL
+for query in SQL.split(";"):
+    df = sqlContext.sql(query)
+
 #############################################################
 # Processing: Filter out products whose labels dont match
 #############################################################
-sqlContext.sql(SQL) \
-    .groupBy("product_id") \
-    .agg(F.collect_list("similar_product_id").alias("similar_product_ids"),
-         F.collect_list("similarity_score").alias("similarity_scores")) \
-    .select("product_id", F.posexplode(
-        F.reverse(F.array_sort(F.arrays_zip("similarity_scores", "similar_product_ids"))),
+df.groupBy("product_id") \
+    .agg(
+        F.collect_list("similar_product_id").alias("similar_product_ids"),
+        F.collect_list("similarity_score").alias("similarity_scores")
+    ) \
+    .select(
+        "product_id",
+        F.posexplode(
+            F.reverse(
+                F.array_sort(
+                    F.arrays_zip(
+                        "similarity_scores",
+                        "similar_product_ids"
+                    )
+                )
+            ),
         )
     ) \
     .select(
-      F.col("product_id"),
-      F.col("pos").alias("index"),
-      F.col("col.similar_product_ids").alias("similar_product_id"),
-      F.col("col.similarity_scores").alias("similarity_score")
-  ) \
-  .where(F.col("index") < TOP_N) \
-  .write.saveAsTable(OUTPUT_TABLE, format="delta", mode="overwrite")
+        F.col("product_id"),
+        F.col("pos").alias("index"),
+        F.col("col.similar_product_ids").alias("similar_product_id"),
+        F.col("col.similarity_scores").alias("similarity_score")
+    ) \
+    .where(F.col("index") < TOP_N) \
+    .write.saveAsTable(OUTPUT_TABLE, format="delta", mode="append")
