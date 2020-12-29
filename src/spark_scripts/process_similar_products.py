905cb8e2fb785fc09b203a6d28d3e9e6c90279fd
ghodouss <ghodouss@usc.edu>
2020-12-29 09:55:50 -0600

    clean up proc similar items and add execution date param

--- src/spark_scripts/process_similar_products.py
+++ src/spark_scripts/process_similar_products.py
@@ -2,6 +2,7 @@ import argparse
 import json
 
 from pyspark.sql import SQLContext, SparkSession
+from pyspark.sql.types import DateType
 from pyspark import SparkContext
 
 # Databricks notebook source
@@ -16,6 +17,7 @@ with open(args.json, "rb") as handle:
 OUTPUT_TABLE = json_args["output_table"]
 TOP_N = json_args["TOP_N"]
 SQL = json_args["sql"]
+DS = json_args["DS"]
 
 ## Hack for linter
 try:
@@ -26,12 +28,22 @@ except Exception:
     pass
 
 ## Run SQL
+## IMPORTANT!! This deletes product ids that
+## we are going to overwrite so we can append
 for query in SQL.split(";"):
     df = sqlContext.sql(query)
 
 #############################################################
 # Processing: Filter out products whose labels dont match
 #############################################################
+def sorted_posexplode(score_field: str, additional_fields: list, asc: bool = True
+        ) -> F.col:
+    fields = [score_field] + additional_fields
+    zipped_fields = F.arrays_zip(*fields) # score first for sorting
+    sorted_fields = F.array_sort(zipped_fields)
+    correctly_sorted_fields = sorted_fields if asc else F.reverse(sorted_fields)
+    return F.posexplode(correctly_sorted_fields)
+
 df.groupBy("product_id") \
     .agg(
         F.collect_list("similar_product_id").alias("similar_product_ids"),
@@ -39,15 +51,10 @@ df.groupBy("product_id") \
     ) \
     .select(
         "product_id",
-        F.posexplode(
-            F.reverse(
-                F.array_sort(
-                    F.arrays_zip(
-                        "similarity_scores",
-                        "similar_product_ids"
-                    )
-                )
-            ),
+        sorted_posexplode(
+            "similarity_scores",
+            "similar_product_ids",
+            asc=False
         )
     ) \
     .select(
@@ -56,5 +63,11 @@ df.groupBy("product_id") \
         F.col("col.similar_product_ids").alias("similar_product_id"),
         F.col("col.similarity_scores").alias("similarity_score")
     ) \
+    .withColumn(
+        "execution_date",
+        F.lit(DS).cast(DateType())
+    ) \
     .where(F.col("index") < TOP_N) \
-    .write.saveAsTable(OUTPUT_TABLE, format="delta", mode="append")
+    .write \
+    .option("mergeSchema", "true") \
+    .saveAsTable(OUTPUT_TABLE, format="delta", mode="append")
