47d1c79a2edec4d0fca550b4578ade7ba21dec74
ghodouss <ghodouss@usc.edu>
2021-01-06 20:49:53 -0600

    added default_search_order template for product search upload

---
+++ src/template/process_product_search_products.sql
@@ -0,0 +1,12 @@
+WITH advertiser_count AS (
+  SELECT advertiser_name, count(*) as n_products
+  FROM {{ params.active_products_table }} 
+  GROUP BY advertiser_name
+)
+SELECT 
+  ap.*,
+  random()*log10(ac.n_products) as default_search_order,
+  (n_likes + n_add_to_cart + 1) / n_views as swipe_rate
+FROM {{ params.active_products_table }} ap 
+INNER JOIN advertiser_count ac
+ON ap.advertiser_name=ac.advertiser_name
