8eca133eb499a07cd67307f0410225d7469f40a9
Rami <rhammoud@usc.edu>
2021-01-15 12:59:40 -0800

    Revert "Created initial schema for user faves"
    This reverts commit 0bef78b8d509fe961ea185624c3a350fa7f080d0.

--- src/defs/postgre/user_data.py
+++ src/defs/postgre/user_data.py
@@ -4,7 +4,6 @@ definitions for bigquery
 tables.
 """
 import os
-from src.defs.postgre import product_catalog as pcdefs
 
 PROJECT = os.environ.get("PROJECT", "staging")
 INSTANCE = "fleek-app-prod1"
@@ -13,7 +12,6 @@ CONN_ID = f'google_cloud_sql_{DATABASE}'
 BQ_EXTERNAL_CONN_ID = "fleek-prod.us.cloudsql_ktest"
 
 USER_EVENTS_TABLE = "user_events"
-USER_FAVES_TABLE = "user_faves"
 
 def get_full_name(table_name, staging=False):
     if staging:
@@ -76,30 +74,4 @@ SCHEMAS = {
         ],
         "tail" : f";\nCREATE INDEX IF NOT EXISTS {PROJECT}_user_events_table_index ON {get_full_name(USER_EVENTS_TABLE)} (event_timestamp)"
     },
-    USER_FAVES_TABLE: {
-        "schema" : [
-            {
-                "name": "user_id",
-                "type": "bigint",
-                "mode": "NOT NULL"
-            },
-            {
-                "name": "product_id",
-                "type": "bigint",
-                "mode": "NOT NULL",
-                "prod": (
-                    f"REFERENCES {pcdefs.get_full_name(pcdefs.PRODUCT_INFO_TABLE)} (product_id)"
-                )
-            },
-            {
-                "name": "event_timestamp",
-                "type": "bigint",
-                "mode": "NOT NULL",
-                "prod": (
-                    f"constraint pk_{PROJECT}_{USER_FAVES_TABLE} primary key (user_id, product_id)"
-                )
-            },
-        ],
-        "tail" : f";\nCREATE INDEX IF NOT EXISTS {PROJECT}_{USER_FAVES_TABLE}_index ON {get_full_name(USER_FAVES_TABLE)} (user_id, event_timestamp)"
-    }
 }
