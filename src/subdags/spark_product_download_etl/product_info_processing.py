1ba534a13f0c5d8c0c12d49164329053ea3745fe
ghodouss <ghodouss@usc.edu>
2021-01-26 14:12:11 -0600

    basic prod info proc setup

--- src/subdags/spark_product_download_etl/product_info_processing.py
+++ src/subdags/spark_product_download_etl/product_info_processing.py
@@ -20,6 +20,22 @@ def get_operators(dag: DAG_TYPE) -> TaskGroup:
 
     with TaskGroup(group_id="product_info_processing", dag=dag) as group:
 
+        SparkSQLOperator(
+            dag=dag,
+            task_id="basic_processing",
+            json_args={
+                "src_table": pcdefs.get_full_name(pcdefs.DAILY_PRODUCT_DUMP_TABLE),
+                "output_table": pcdefs.get_full_name(pcdefs.PRODUCT_INFO_TABLE),
+                "ds": "{{ds}}",
+                "timestamp": "{{ execution_date.int_timestamp }}",
+                "drop_kwargs_path": f"{DBFS_DEFS_DIR}/product_download/global/drop_keywords.json" \
+                        .replace("dbfs:", "/dbfs"),
+                "labels_path": f"{DBFS_DEFS_DIR}/product_download/global/product_labels.json" \
+                        .replace("dbfs:", "/dbfs")
+            },
+            local=True,
+        )
+
         product_info_processing = SparkScriptOperator(
             dag=dag,
             task_id="product_info_processing",
