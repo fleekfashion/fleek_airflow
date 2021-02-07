b08a6fa78b9840ce82f7560f76a8d6d4987c0617
ghodouss <ghodouss@usc.edu>
2021-02-07 17:38:08 -0600

    migrated spark personalization schema

--- src/defs/postgre/spark_personalization.py
+++ src/defs/postgre/spark_personalization.py
@@ -4,48 +4,39 @@ definitions for postgre
 tables.
 """
 import os
+
+import sqlalchemy as sa
+
 from src.defs.postgre import product_catalog as pcdefs
+from src.defs.utils import PostgreTable, POSTGRES_METADATA, build_staging_table
 
 PROJECT = os.environ.get("PROJECT", "staging")
 INSTANCE = "fleek-app-prod1"
 DATABASE = "ktest"
 CONN_ID = f'google_cloud_sql_{DATABASE}'
-BQ_EXTERNAL_CONN_ID = "fleek-prod.us.cloudsql_ktest"
 
-USER_PRODUCT_RECS_TABLE = "user_product_recommendations"
+USER_PRODUCT_RECS_TABLE_NAME = "user_product_recommendations"
 
-def get_full_name(table_name, staging=False):
-    if staging:
-        table_name = "staging_" + table_name
-    return f"{PROJECT}.{table_name}"
-
-def get_columns(table_name):
-    schema = SCHEMAS.get(table_name)['schema']
-    return [ c['name'] for c in schema ]
-
-SCHEMAS = {
-    USER_PRODUCT_RECS_TABLE: {
-        "schema" : [
-            {
-                "name": "user_id",
-                "type": "bigint",
-                "mode": "NOT NULL",
-            },
-            {
-                "name": "index",
-                "type": "bigint",
-                "mode": "NOT NULL"
-            },
-            {
-                "name": "product_id",
-                "type": "bigint",
-                "mode": f"NOT NULL",
-                "prod": (
-                    f"REFERENCES {pcdefs.get_full_name(pcdefs.PRODUCT_INFO_TABLE)} (product_id),\n"
-                    f"constraint pk_{PROJECT}_user_product_recs primary key (user_id, index)"
-                )
-            },
-    ],
-        "tail": f";\nCREATE INDEX IF NOT EXISTS {PROJECT}_user_product_recs_index ON {get_full_name(USER_PRODUCT_RECS_TABLE)} (user_id, index)",
-    }
-}
+USER_PRODUCT_RECS_TABLE = PostgreTable(
+    USER_PRODUCT_RECS_TABLE_NAME,
+    POSTGRES_METADATA,
+    sa.Column(
+        "user_id",
+        sa.BIGINT,
+        nullable=False
+        ),
+    sa.Column(
+        "index",
+        sa.BIGINT,
+        nullable=False
+        ),
+    sa.Column(
+        "product_id",
+        sa.BIGINT,
+        nullable=False,
+        foreign_key=sa.ForeignKey(f"{pcdefs.PRODUCT_INFO_TABLE}.product_id")
+        ),
+    sa.PrimaryKeyConstraint("user_id", "index",),
+    sa.Index(f"{USER_PRODUCT_RECS_TABLE_NAME}_user_id_index_ix_", "user_id", "index")
+)
+USER_PRODUCT_RECS_TABLE_STAGING = build_staging_table(USER_PRODUCT_RECS_TABLE)
