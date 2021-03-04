38ae7f732de0c1174d349fc16d69ce73698ae6fd
Kian <ghodouss@usc.edu>
2021-03-03 18:07:12 -0800

    smart name processing

--- src/template/process_product_name.sql
+++ src/template/process_product_name.sql
@@ -1,24 +1,38 @@
 CREATE OR REPLACE TEMPORARY VIEW pinfo AS (
   SELECT DISTINCT
     product_id,
-    product_name,
+    aggregate(
+      array(
+        'MANGO -'
+      ),
+      product_name,
+      (acc, x) -> trim(regexp_replace(acc, x, ''))
+    ) as product_name,
     color,
-    size
+    COALESCE(size, 'absurd_impossible_string_tthat_means_nothing') as size -- b/c size can be null
   FROM {{params.product_info_table}} 
 );
 
 WITH t AS (
   SELECT
     product_id,
-    regexp_replace(
-      regexp_replace(
-        product_name,
-        COALESCE(size, 'absurd_impossible_string_tthat_means_nothing'), -- b/c size can be null
-        ''
+    aggregate(
+      array(
+        ' - .*',
+        '\\..*',
+        '(?i)\\bextra\\b', 
+        '(?i)\\bx\\b', 
+        '(?i)\\bsmall\\b', 
+        '(?i)\\bmedium\\b', 
+        '(?i)\\blarge\\b', 
+        array_join(ARRAY('(?i)\\b', size, '\\b'), ''),
+        '(?i)\\bsize$',
+        array_join(ARRAY('(?i)\\bin ', color, '.*'), ''),
+        array_join(ARRAY('(?i)-.', color, '.*'), '')
       ),
-      ' - .*', 
-      ''
-    ) as product_name 
+      product_name,
+      (acc, x) -> trim(regexp_replace(acc, x, ''))
+    ) as product_name
   FROM pinfo
 )
 
