f7042b5d8c645afb3497230d166200da67f04e50
Rami Hammoud <rhammoud@usc.edu>
2021-03-15 22:26:48 -0700

    Add user events ground truth tables (#77)
    * Add user events ground truth tables
    
    * Fix nit for user events ground truth tables
    
    * Change names of user events ground truth endpoint tables

--- src/defs/postgre/user_data.py
+++ src/defs/postgre/user_data.py
@@ -5,6 +5,7 @@ tables.
 """
 import os
 from src.defs.postgre.utils import *
+from src.defs.postgre import product_catalog as pcdefs
 
 PROJECT = os.environ.get("PROJECT", "staging")
 INSTANCE = "fleek-app-prod1"
@@ -13,6 +14,9 @@ CONN_ID = f'google_cloud_sql_{DATABASE}'
 BQ_EXTERNAL_CONN_ID = "fleek-prod.us.cloudsql_ktest"
 
 USER_EVENTS_TABLE_NAME = "user_events"
+USER_PRODUCT_FAVES_TABLE_NAME = "user_product_faves"
+USER_PRODUCT_BAGS_TABLE_NAME = "user_product_bags"
+USER_PRODUCT_SEENS_TABLE_NAME = "user_product_seens"
 
 USER_EVENTS_TABLE = PostgreTable(
     name=USER_EVENTS_TABLE_NAME,
@@ -65,6 +69,117 @@ USER_EVENTS_TABLE = PostgreTable(
     ]
 )
 
+USER_PRODUCT_FAVES_TABLE = PostgreTable(
+    name=USER_PRODUCT_FAVES_TABLE_NAME,
+    columns=[
+        Column(
+            "user_id",
+            "bigint",
+            nullable=False
+        ),
+        Column(
+            "product_id",
+            "bigint",
+            nullable=False,
+        ),
+        Column(
+            "event_timestamp",
+            "bigint",
+            nullable=False
+        )
+    ],
+    primary_key=PrimaryKey(
+        ["user_id", "product_id"],
+    ),
+    foreign_keys=[
+        ForeignKey(
+            columns=["product_id"],
+            ref_table=pcdefs.PRODUCT_INFO_TABLE.get_full_name(),
+            ref_columns=["product_id"]
+        )
+    ],
+    indexes=[
+        Index(
+            ["user_id", "event_timestamp"],
+        )
+    ]
+)
+
+USER_PRODUCT_BAGS_TABLE = PostgreTable(
+    name=USER_PRODUCT_BAGS_TABLE_NAME,
+    columns=[
+        Column(
+            "user_id",
+            "bigint",
+            nullable=False
+        ),
+        Column(
+            "product_id",
+            "bigint",
+            nullable=False,
+        ),
+        Column(
+            "event_timestamp",
+            "bigint",
+            nullable=False
+        )
+    ],
+    primary_key=PrimaryKey(
+        ["user_id", "product_id"],
+    ),
+    foreign_keys=[
+        ForeignKey(
+            columns=["product_id"],
+            ref_table=pcdefs.PRODUCT_INFO_TABLE.get_full_name(),
+            ref_columns=["product_id"]
+        )
+    ],
+    indexes=[
+        Index(
+            ["user_id", "event_timestamp"],
+        )
+    ]
+)
+
+USER_PRODUCT_SEENS_TABLE = PostgreTable(
+    name=USER_PRODUCT_SEENS_TABLE_NAME,
+    columns=[
+        Column(
+            "user_id",
+            "bigint",
+            nullable=False
+        ),
+        Column(
+            "product_id",
+            "bigint",
+            nullable=False,
+        ),
+        Column(
+            "event_timestamp",
+            "bigint",
+            nullable=False
+        )
+    ],
+    primary_key=PrimaryKey(
+        ["user_id", "product_id"],
+    ),
+    foreign_keys=[
+        ForeignKey(
+            columns=["product_id"],
+            ref_table=pcdefs.PRODUCT_INFO_TABLE.get_full_name(),
+            ref_columns=["product_id"]
+        )
+    ],
+    indexes=[
+        Index(
+            ["user_id", "event_timestamp"],
+        )
+    ]
+)
+
 TABLES.extend([
-    USER_EVENTS_TABLE
+    USER_EVENTS_TABLE,
+    USER_PRODUCT_FAVES_TABLE,
+    USER_PRODUCT_BAGS_TABLE,
+    USER_PRODUCT_SEENS_TABLE
 ])
