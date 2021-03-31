c1e1d3a18cc095b72a37f5052dbd8c8fddcb9545
Kian Ghodoussi <ghodouss@usc.edu>
2021-03-31 11:41:24 -0700

    Revert "increase search brand normalization (#104)" (#106)
    This reverts commit 9d0aadea3626d18e565976b8aa7da44f936b3f23.

--- src/template/process_product_search_products.sql
+++ src/template/process_product_search_products.sql
@@ -5,7 +5,7 @@ WITH advertiser_count AS (
 )
 SELECT 
   ap.*,
-  random()*sqrt(ac.n_products)*cbrt(ac.n_products)*sqrt(sqrt(ac.n_products)) as default_search_order,
+  random()*sqrt(ac.n_products)*cbrt(ac.n_products) as default_search_order,
   (n_likes + n_add_to_cart + 1) / n_views as swipe_rate
 FROM {{ params.active_products_table }} ap 
 INNER JOIN advertiser_count ac
