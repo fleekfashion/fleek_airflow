72ea61c5897fc5ddcb798543ab803ea1fa7a3d0c
Kian Ghodoussi <ghodouss@usc.edu>
2021-05-17 14:55:15 -0700

    Prod colors no sim items (#138)
    * reduce product labels filter strength
    
    * rm product labels and secondary as filter

--- src/template/generate_product_color_options.sql
+++ src/template/generate_product_color_options.sql
@@ -38,32 +38,23 @@ CREATE OR REPLACE TEMP VIEW parsed_active AS (
 );
 
 CREATE OR REPLACE TEMPORARY VIEW similar_product_info AS (
-  
   WITH t AS (
     SELECT 
-      si.product_id,
-      si.similar_product_id,
+      a1.product_id,
+      a2.product_id as similar_product_id,
       a1.split_product_name as root_product_name,
       a2.split_product_name as similar_product_name,
       a1.color as root_color,
       a2.color as similar_color,
       a1.product_image_url as root_url,
       a2.product_image_url as similar_url
-    FROM prod_product_catalog.similar_products_v2 si
-    INNER JOIN parsed_active a1
-      ON si.product_id = a1.product_id
+    FROM parsed_active a1
     INNER JOIN parsed_active a2
-      ON si.similar_product_id = a2.product_id
-    WHERE si.product_id IN (
-      SELECT product_id 
-      FROM prod_product_catalog.active_products
-    ) AND a1.advertiser_name = a2.advertiser_name
-    AND size(a1.product_labels) + size(a2.product_labels) = 2*size(array_intersect(a1.product_labels, a2.product_labels))
-    AND size(a1.product_secondary_labels) + size(a2.product_secondary_labels) = 2*size(array_intersect(a1.product_secondary_labels, a2.product_secondary_labels))
-    AND a1.product_id != a2.product_id
-    AND size(a1.split_product_name) = size(a2.split_product_name)
-    AND a1.color != a2.color
-    AND a1.product_brand = a2.product_brand
+      ON a1.advertiser_name = a2.advertiser_name
+      AND a1.product_brand = a2.product_brand
+      AND a1.product_id != a2.product_id
+      AND a1.color != a2.color
+      AND size(a1.split_product_name) = size(a2.split_product_name)
   )
 
   SELECT 
@@ -84,42 +75,29 @@ CREATE OR REPLACE TEMPORARY VIEW final_colors AS (
     SELECT 
       *
     FROM similar_product_info
-    WHERE size(root_product_name) - n_matching_words <= 1
-    AND n_matching_words::float/size(root_product_name) >= .8
+    WHERE size(root_product_name) = n_matching_words
   ), t2 AS (
-    SELECT
-      product_id,
-      max(n_matching_words) as max_matching_words
-    FROM t1
-    GROUP BY product_id
-  ), t3 AS (
-    SELECT
-      t1.*
-    FROM t1
-    INNER JOIN t2
-    ON t1.product_id = t2.product_Id
-    WHERE n_matching_words = max_matching_words
-  ), t4 AS (
     SELECT 
       product_id,
       similar_product_id
-    FROM t3
+    FROM t1
       UNION ALL
     SELECT 
-      t3.product_id as product_id,
+      t1.product_id as product_id,
       tsim.similar_product_id as similar_product_id
-    FROM t3
-    INNER JOIN t3 tmid
-    ON t3.product_id = tmid.similar_product_id
-    INNER JOIN t3 tsim
+    FROM t1
+    INNER JOIN t1 tmid
+    ON t1.product_id = tmid.similar_product_id
+    INNER JOIN t1 tsim
     ON tmid.product_id = tsim.product_id
-    WHERE t3.root_color != tsim.similar_color
+    WHERE t1.root_color != tsim.similar_color
   )
+
 
   SELECT 
     product_id,
     similar_product_id as alternate_color_product_id
-  FROM t4
+  FROM t2
   WHERE product_id != similar_product_id
 );
 
