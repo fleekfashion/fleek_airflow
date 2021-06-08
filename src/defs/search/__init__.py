93cab4e12fbc48b227eacbd20aa2ad53f9b17a57
Kian Ghodoussi <ghodouss@usc.edu>
2021-06-07 22:09:30 -0700

    Update __init__.py

--- src/defs/search/__init__.py
+++ src/defs/search/__init__.py
@@ -1,7 +1,7 @@
 import os
 from src.defs.delta.utils import PROJECT
 
-URL = 'http://159.89.82.234'
+URL = 'http://174.138.109.133/'
 PASSWORD = os.environ["SEARCH_PASSWORD"]
 PRODUCT_SEARCH_INDEX = f"{PROJECT}_products"
 AUTOCOMPLETE_INDEX = f"{PROJECT}_autocomplete"
