b396eb23823635446d6594261ecfac9e41b510c1
Kian <ghodouss@usc.edu>
2021-07-24 10:59:28 -0500

    change min include smart tags to 75

--- src/subdags/spark_product_download_etl/active_products_ml.py
+++ src/subdags/spark_product_download_etl/active_products_ml.py
@@ -150,7 +150,7 @@ def get_operators(dag: DAG_TYPE) -> dict:
             sql="template/build_smart_tags.sql",
             params={
                 "product_smart_tag_table": boards.PRODUCT_SMART_TAG_TABLE.get_full_name(),
-                "min_include": 100
+                "min_include": 75
             },
             local=True,
             output_table=boards.SMART_TAG_TABLE.get_full_name(),
